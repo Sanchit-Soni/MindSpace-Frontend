20ee318dbdfe5b7995b459599c9a1feda8580c37
Sanchit-Soni <sanchitsony@gmail.com>
2021-03-02 16:46:25 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import NewNav from "./components/NewNav";
+import Login from "./components/Login";
+import { useStateValue } from "./StateProvider";
+import Home from "./components/Home";
+import Profile from "./components/Profile";
 
 function App() {
+  const Routing = () => {
+    return (
+      <Switch>
+        <Route path="/" exact component={Home} />
+
+        <Route path="/Home" exact component={Home} />
+        <Route path="/Profile" exact component={Profile} />
+      </Switch>
+    );
+  };
+  const [{ user }, dispatch] = useStateValue();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!user ? (
+        <Login />
+      ) : (
+        <div className="main">
+          <Router>
+            <NewNav />
+
+            <Routing />
+          </Router>
+        </div>
+      )}
     </div>
   );
 }
