40e30d068eba6fe2228e05d45fbbbb8233a435e8
Sanchit-Soni <sanchitsony@gmail.com>
2021-06-17 18:55:25 +0530

    test

--- src/components/Analysis.js
+++ src/components/Analysis.js
@@ -9,8 +9,26 @@ import LineChart from "./Charts/LineChart";
 import PieChart from "./Charts/PieChart";
 const Analysis = () => {
   const [wymData, setWymData] = useState();
+  const [loading, setLoading] = useState(true);
   const [profile, setProfile] = useState({});
-  useEffect(async () => {
+  // useEffect(async () => {
+  //   if (localStorage.getItem("user-details") !== null) {
+  //     let values = localStorage.getItem("user-details");
+  //     let newVal = JSON.parse(values);
+  //     setProfile(newVal.user);
+  //   }
+
+  //   const uid = `${profile.uid}`;
+  //   console.log(uid);
+  //   let des = GetDataById(uid);
+  //   let ses = Getdata(uid);
+  //   console.log(ses);
+  //   console.log(des);
+  //   setWymData(des);
+  //   console.log(wymData);
+  //   console.log(des);
+  // }, [wymData, profile.uid]);
+  useEffect(() => {
     if (localStorage.getItem("user-details") !== null) {
       let values = localStorage.getItem("user-details");
       let newVal = JSON.parse(values);
@@ -18,21 +36,43 @@ const Analysis = () => {
     }
 
     const uid = `${profile.uid}`;
-    console.log(uid);
-    let des = GetDataById(uid);
-    let ses = Getdata(uid);
-    console.log(ses);
-    console.log(des);
-    setWymData(des);
-    console.log(wymData);
-    console.log(des);
-  }, [wymData, profile.uid]);
 
-  return (
+    (async () => {
+      const datas = await GetDataById(uid);
+      if (datas) {
+        setWymData(datas);
+      }
+      console.log(datas);
+      setLoading(false);
+    })();
+    // console.log(uid);
+    // let des = GetDataById(uid);
+    // let ses = Getdata(uid);
+    // console.log(ses);
+    // console.log(des);
+    // setWymData(des);
+    // console.log(des);
+  }, [loading]);
+
+  useEffect(() => {
+    console.log(wymData);
+  }, [wymData]);
+
+  return loading === true ? (
+    <h1>Loading</h1>
+  ) : (
     <div className="container">
       <h1>Analytics</h1>
       <div>
         <Table />
+      </div>
+      <div>
+        {wymData !== null && <h1>Data exist</h1>}
+        {/* {wymData.map((des) => (
+          <>
+            <p>{des.id}</p>
+          </>
+        ))} */}
       </div>
       {/* <div className="charts">
         <div className="chart-1">
